f9f97a68781700774410ee157a66032337935890
kingki <kingki_n@etna-alternance.net>
2026-02-07 15:10:23 +0100

    security: move credentials to environment variables

--- backend/main.py
+++ backend/main.py
@@ -71,14 +71,14 @@ def metrics():
 # Client S3
 s3_client = boto3.client(
     's3',
-    endpoint_url='http://localhost:4566',
-    aws_access_key_id='test',
-    aws_secret_access_key='test',
-    region_name='us-east-1'
+    endpoint_url=os.getenv('LOCALSTACK_ENDPOINT', 'http://localhost:4566'),
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID', 'test'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY', 'test'),
+    region_name=os.getenv('AWS_REGION', 'us-east-1')
 )
 
 # Nom du bucket
-BUCKET_NAME = 'cloud-photo-bucket'
+BUCKET_NAME = os.getenv('S3_BUCKET_NAME', 'cloud-photo-bucket')
 
 
 # Upload image
